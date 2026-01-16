10ed6280391161154c393cfb7804c3ec865f9567
Rehan Shaikh <skrehan1117@gmail.com>
2026-01-16 13:18:44 +0530

    add react and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-center pt-20 text-lg text-blue-800">Hello Team</h1>
+    </div>
+  );
+};
+
+export default App;

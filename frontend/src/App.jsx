d2535a17b6cf238641de0a0fa9d89cd80e96ef0c
Rehan Shaikh <skrehan1117@gmail.com>
2026-01-16 14:49:25 +0530

    add the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 
-const App = () => {
+const AppContent = () => {
   return (
-    <div>
-      <h1 class="text-center pt-20 text-lg text-blue-800">Hello Team</h1>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
     </div>
   );
 };
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
 
 export default App;
